203fdd824f2460e13b01ba511e8b9aef95011551
Damian Bruno <damianbruno17@gmail.com>
2025-02-25 13:37:23 -0300

    feat: initialize library with Book constructor
    Add myLibrary array to store all Book instances and create a Book
    constructor that takes user-provided values (title, author, pages,
    status) to instantiate book objects.

---
+++ script.js
@@ -0,0 +1,10 @@
+//array to contain every book of the library
+const myLibrary = []
+
+function Book(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
