bbe1dfa644a8bcb7d5538f1beb5907bdf7940527
Damian Bruno <damianbruno17@gmail.com>
2025-02-25 14:54:42 -0300

    feat: add book display functionality
    Implement displayBooks to iterate through myLibrary and call
    createBookCard for each book, which generates HTML elements to
    render book details (title, author, pages, status) in the DOM.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //array to contain every book of the library
 const myLibrary = []
+const cardsContainer = document.querySelector('.books-container');
 
 function Book(title, author, pages, status) {
     this.title = title;
@@ -11,5 +12,34 @@ function Book(title, author, pages, status) {
 function addBookToLibrary(title, author, pages, status) {
     const newBook = new Book(title, author, pages, status);
     myLibrary.push(newBook);
+    displayBooks();
 }
 
+function displayBooks() {
+    myLibrary.forEach((book) => {
+        createBookCard(book.title, book.author, book.pages, book.status);
+    })
+}
+
+function createBookCard(title, author, pages, status) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const bookTitle = document.createElement('h3');
+    bookTitle.textContent = title;
+
+    const bookAuthor = document.createElement('p');
+    bookAuthor.textContent = `Author: ${author}`;
+
+    const bookPages = document.createElement('p');
+    bookPages.textContent = `Pages: ${pages}`;
+
+    const bookStatus = document.createElement('p');
+    bookStatus.textContent = `Status: ${(status) ? "Already read" : "Not read yet"}`;
+
+    cardsContainer.appendChild(card);
+    card.appendChild(bookTitle);
+    card.appendChild(bookAuthor);
+    card.appendChild(bookPages);
+    card.appendChild(bookStatus);
+}
