e61532a8ae02f4e049003d08ede07544fbf3d5e8
Damian Bruno <damianbruno17@gmail.com>
2025-02-26 20:32:26 -0300

    feat: add dialog interaction and form submission logic
    Introduce selectors and event listeners for .show-dialog to open
    the modal and .add-book to submit form data to addBookToLibrary,
    closing the dialog and resetting the form after submission.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //array to contain every book of the library
 const myLibrary = []
 const cardsContainer = document.querySelector('.books-container');
+const dialog = document.querySelector('.dialog');
+const form = document.querySelector('.new-book-form');
+const showDialogButton = document.querySelector('.show-dialog');
+const closeDialogButotn = document.querySelector('.close');
+const addBookButton =  document.querySelector('.add-book');
 
 function Book(title, author, pages, status) {
     this.title = title;
@@ -36,7 +41,7 @@ function createBookCard(title, author, pages, status) {
     bookPages.textContent = `Pages: ${pages}`;
 
     const bookStatus = document.createElement('p');
-    bookStatus.textContent = `Status: ${(status) ? "Already read" : "Not read yet"}`;
+    bookStatus.textContent = `Status: ${(status == 'true') ? "Already read" : "Not read yet"}`;
 
     cardsContainer.appendChild(card);
     card.appendChild(bookTitle);
@@ -46,10 +51,29 @@ function createBookCard(title, author, pages, status) {
 }
 
 
+showDialogButton.addEventListener('click', () => {
+    dialog.showModal()
+    
+})
+
+addBookButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    
+    const bookTitle = document.querySelector('#title').value.trim();
+    const bookAuthor = document.querySelector('#author').value.trim();
+    const bookPages = document.querySelector('#pages').value.trim();
+    const bookStatus = document.querySelector('input[name="status"]:checked').value;
+    
+    addBookToLibrary(bookTitle, bookAuthor, bookPages, bookStatus);
+
+    dialog.close()
+    form.reset();
+})
+
 // Dummy content
-addBookToLibrary("The Hobbit", "J. R. R. Tolkien", 310, false);
-addBookToLibrary("A Princess of Mars", "Edgar Rice Burroughs", 326, true);
-addBookToLibrary("Siddhartha", "Hermann Hesse", 152, true);
-addBookToLibrary("Animal Farm", "George Orwell", 92, true);
-addBookToLibrary("1984", "George Orwell", 326, false);
-addBookToLibrary("1984", "George Orwell", 326, false);
+addBookToLibrary("The Hobbit", "J. R. R. Tolkien", 310, "false");
+addBookToLibrary("A Princess of Mars", "Edgar Rice Burroughs", 326, "true");
+addBookToLibrary("Siddhartha", "Hermann Hesse", 152, "true");
+addBookToLibrary("Animal Farm", "George Orwell", 92, "true");
+addBookToLibrary("1984", "George Orwell", 326, "false");
+addBookToLibrary("1984", "George Orwell", 326, "false");
